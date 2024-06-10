c98383740f81656e1913193c1883351e91052264
Andrew Burrows <andrew.burrows2@sky.uk>
2024-06-10 04:39:08 +0100

    Modifies cosine similarity rerank using a sentence transformer embedding rather than an LLM embedding.

--- llm/context_retrieval/context_retrievers/cosine_similarity_rerank.py
+++ llm/context_retrieval/context_retrievers/cosine_similarity_rerank.py
@@ -1,15 +1,12 @@
 from langchain.prompts import ChatPromptTemplate
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.prompts import ChatPromptTemplate
 from langchain.schema import Document
 from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.vectorstores import VectorStoreRetriever
 import numpy as np
-from langchain_community.embeddings import OllamaEmbeddings
 
+from langchain_community.embeddings.sentence_transformer import (SentenceTransformerEmbeddings, )
 from llm.context_retrieval.context_retriever import ContextRetriever
-from operator import itemgetter
 
 """
 Re-rank multi-query retrieved documents based on the cosine similarity with the original input.
@@ -20,7 +17,7 @@ class CosineSimilarityRerank(ContextRetriever):
     def __init__(self, retriever: VectorStoreRetriever, multi_query_request: str):
         super().__init__(retriever)
         self.multi_query_request = multi_query_request
-        self.embeddings = OllamaEmbeddings(model="llama3")  # Initialize the embeddings model
+        self.embeddings_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
     def query_context_chain(self):
         prompt_rag_fusion = ChatPromptTemplate.from_template(self.multi_query_request)
@@ -72,14 +69,14 @@ class CosineSimilarityRerank(ContextRetriever):
 
         def generate_responses(queries: list[str]) -> list[(Document, float)]:
             documents = []
-            question_embedding = self.embeddings.embed_query(question)  # Embed the original question
+            question_embedding = self.embeddings_function.embed_query(question)  # Embed the original question
             for query in queries:
                 docs_with_scores = self.retriever.similarity_search_with_relevance_scores(query=query, k=4)
                 print(f"\nDocuments retrieved for '{query}'")
                 for doc, score in docs_with_scores:
                     print(f"score: {score}, doc: {doc}")
                 for doc, score in docs_with_scores:
-                    doc_embedding = self.embeddings.embed_query(doc.page_content)  # Embed the document content
+                    doc_embedding = self.embeddings_function.embed_query(doc.page_content)  # Embed the document content
                     relevance_score = np.dot(question_embedding, doc_embedding)  # Calculate the semantic similarity
                     documents.append((doc, relevance_score))
 
@@ -88,14 +85,15 @@ class CosineSimilarityRerank(ContextRetriever):
         return generate_queries | print_queries | generate_responses | deduplicate | self._reciprocal_rank_fusion
 
     def _reciprocal_rank_fusion(self, retrieved_documents: list[(Document, float)]):
-        sorted_documents = sorted(retrieved_documents, key=lambda document: document[1])
+        sorted_documents = sorted(retrieved_documents, key=lambda document: document[1], reverse=True)
         print(f"-----| Documents retrieved |-----")
         for document, score in retrieved_documents:
-            print(f"{score}: {document}")
+            print(f"{score}: {document.page_content}")
         if len(sorted_documents) > 6:
             response = [page_content for page_content, _ in sorted_documents[:6]]
         else:
             response = [page_content for page_content, _ in sorted_documents]
         print(f"-----| Filtered ranked results |-----")
-        print(response)
+        for document in response:
+            print(f"{document.page_content}")
         return response
