eee4d4ea9061c15d14fe05a011e63b781ef62398
Andrew Burrows <andrew.burrows2@sky.uk>
2024-06-11 13:30:42 +0100

    Adds genie avatar.

--- web.py
+++ web.py
@@ -11,14 +11,19 @@ from llm.config_loader import load_yaml
 from llm.context_retrieval.context_retriever_factory import ContextRetrieverFactory
 from llm.vector_store import VectorStore
 
-
 config: dict = load_yaml('llm-config.yml')
 vector_store = VectorStore()
 retriever = vector_store.load_store()
 context_retriever = ContextRetrieverFactory(config).create_context_retriever(retriever)
 
+
 @cl.on_chat_start
 async def on_chat_start():
+    await cl.Avatar(
+        name="Genie",
+        url="http://localhost:8000/public/img.png",
+    ).send()
+
     prompt = ChatPromptTemplate.from_template(config['llm-query-template'])
     final_rag_chain = (
             {"context": context_retriever.query_context_chain(),
@@ -33,7 +38,7 @@ async def on_chat_start():
 @cl.on_message
 async def on_message(message: cl.Message):
     runnable = cl.user_session.get("answerer")
-    msg = cl.Message(content="")
+    msg = cl.Message(content="", author="Genie")
     async for chunk in runnable.astream(
             {"question": message.content},
             config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
